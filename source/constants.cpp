b8bdbc3efe0f14a3c957a2c42439bc06d023c698
archshift <gh@archshift.com>
2015-04-05 20:16:33 -0700

    Fixed the service check patch trying to patch the KProcess* instead of the PID

--- source/constants.cpp
+++ source/constants.cpp
@@ -5,6 +5,7 @@ u32 kproc_start = 0;
 u32 kproc_size  = 0;
 u32 kproc_num   = 0;
 u32 kproc_codeset_offset = 0;
+u32 kproc_pid_offset     = 0;
 
 bool GetVersionConstants()
 {
@@ -16,37 +17,53 @@ bool GetVersionConstants()
     if (!is_n3ds || kversion < 0x022C0600) {
         switch (kversion) {
             case 0x02220000: // 2.34-0 4.1.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02230600: // 2.35-6 5.0.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02240000: // 2.36-0 5.1.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02250000: // 2.37-0 6.0.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02260000: // 2.38-0 6.1.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02270400: // 2.39-4 7.0.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x02280000: // 2.40-0 7.2.0
+                kmem_start  = 0xEFF80000;
                 kproc_size  = 0x260;
                 kproc_codeset_offset = 0xA8;
+                kproc_pid_offset     = 0xAC;
                 break;
             case 0x022C0600: // 2.44-6 8.0.0
+                kmem_start  = 0xDFF80000;
                 kproc_size  = 0x268;
                 kproc_num   = 0x2C;
                 kproc_codeset_offset = 0xB0;
+                kproc_pid_offset     = 0xB4;
                 break;
             case 0x022E0000: // 2.26-0 9.0.0
                 kmem_start  = 0xDFF80000;
@@ -54,19 +71,24 @@ bool GetVersionConstants()
                 kproc_size  = 0x268;
                 kproc_num   = 0x2C;
                 kproc_codeset_offset = 0xB0;
+                kproc_pid_offset     = 0xB4;
                 return true;
         }
     } else {
         switch (kversion) {
             case 0x022C0600: // N3DS 2.44-6 8.0.0
-                kproc_size = 0x270;
-                kproc_num = 0x2F;
+                kmem_start  = 0xDFF80000;
+                kproc_size  = 0x270;
+                kproc_num   = 0x2F;
                 kproc_codeset_offset = 0xB8;
+                kproc_pid_offset     = 0xBC;
                 break;
             case 0x022E0000: // N3DS 2.26-0 9.0.0
+                kmem_start  = 0xDFF80000;
                 kproc_size = 0x270;
                 kproc_num = 0x2F;
                 kproc_codeset_offset = 0xB8;
+                kproc_pid_offset     = 0xBC;
                 break;
         }
     }
--- source/patches_asm.s
+++ source/patches_asm.s
@@ -8,7 +8,7 @@ ret: .word 0
 @@-----------------------------------------------------------------------------
 
 .extern self_pid
-.extern kproc_codeset_offset
+.extern kproc_pid_offset
 
 .global PatchPid
 .type PatchPid, %function
@@ -19,7 +19,7 @@ PatchPid:
 
     ldr r1, =0xFFFF9004          @@ Load pointer to current KProcess
     ldr r1, [r1]
-    ldr r2, =kproc_codeset_offset
+    ldr r2, =kproc_pid_offset
     ldr r2, [r2]
 
     str r0, [r1, r2]             @@ *CURR_KPROC_PTR = 0
@@ -35,7 +35,7 @@ UnpatchPid:
     ldr r0, [r0]
     ldr r1, =self_pid
     ldr r1, [r1]
-    ldr r2, =kproc_codeset_offset
+    ldr r2, =kproc_pid_offset
     ldr r2, [r2]
 
     str r1, [r0, r2]            @@ *CURR_KPROC_PTR = self_pid
